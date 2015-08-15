897bdd5b5f9aa2163a294244c48f77eea6937df0
Josh Phillips <jephillips@cohesionforce.com>
2015-08-15 09:41:53 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Joshua on 8/15/2015.
+ */
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000))
+
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', function(request, response) {
+    response.render('/client/index.html')
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Listening on port ', app.get('port'));
+});
