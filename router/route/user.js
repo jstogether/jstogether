fa439dcd55a837bb070563059049a544b18d05b4
Alex McMillan <alexmcmillan23@gmail.com>
2015-08-20 00:27:53 +1200

    Adding new server-side GET route for retrieving user data

--- router/route/user.js
+++ router/route/user.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 import db from '../../db';
 
+let User = db.model('User');
 let router = express.Router();
 
 
 // Get User by username
 router.get('/:username', (req, res) => {
-	let User = db.model('User');
 
 	User.findOne({
 		username: req.params.username
@@ -17,6 +17,18 @@ router.get('/:username', (req, res) => {
 		if (!user) return res.sendStatus(404);
 
 		return res.send(user.toClient());
+	});
+});
+
+
+// Get existing users
+router.get('/', (req, res) => {
+	User.find((err, users) => {
+		if (err) return res.sendStatus(500);
+		if (!users) return res.sendStatus(404);
+
+		users = users.map(user => user.toClient());
+		return res.send(users);
 	});
 });
 
