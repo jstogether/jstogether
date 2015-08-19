d7c70c85605e6cc36e4e5845ff77ebe9f5fd41c9
Alex McMillan <alexmcmillan23@gmail.com>
2015-08-19 16:49:42 +1200

    Updating config for heroku

--- db/index.js
+++ db/index.js
@@ -3,7 +3,13 @@ import mongoose from 'mongoose';
 import UserSchema from './schema/user';
 import ProjectSchema from './schema/project';
 
-let connection = mongoose.createConnection('127.0.0.1', 'jstogether-tools', 27017);
+import config from '../config';
+
+let connection = mongoose.createConnection(
+	config.get('database:host'),
+	config.get('database:name'),
+	config.get('database:port')
+);
 
 connection.model('User', UserSchema);
 connection.model('Project', ProjectSchema);
