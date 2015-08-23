758aa9cdad6a8ca5d7a15e1d460ce7f1fd93d943
Alex McMillan <alexmcmillan23@gmail.com>
2015-08-23 14:09:39 +1200

    Typo

--- api/project.js
+++ api/project.js
@@ -39,9 +39,9 @@ export default {
 	/**
 	 *
 	 */
-	createTeam (projectId, teamName) {
+	createTeam (projectId, team) {
 		return $.post(`/project/${projectId}/team`, {
-			teamName
+			team
 		});
 	}
 }
