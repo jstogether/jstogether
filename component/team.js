3c527a2af3bfec280ccd43fa2216149bc756ca30
Alex McMillan <alexmcmillan23@gmail.com>
2015-08-24 02:11:23 +1200

    Updating the `Team` component.  Each Team now displays a github URL, a deployed project URL and a list of Team Members

--- component/team.js
+++ component/team.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Component from './component';
 
 import UserShort from './userShort';
+import UserStore from '../store/user';
 
 export default class Team extends Component {
 	/**
@@ -9,14 +10,25 @@ export default class Team extends Component {
 	 */
 	render () {
 		let team = this.props.team;
-		let users = team.users.map(user => <UserShort user={user} />);
+		let users = UserStore.getMulti(team.users).map(user => <UserShort user={user} />);
 
 		return (
 			<div className='team'>
 				<h3>{team.name}</h3>
-				<span>{'Github:'}</span><span>{team.githubUrl}</span>
-				<span>{'Deployed:'}</span><span>{team.deployedUrl}</span>
-				{users}
+				<table className='keyvalue'>
+					<tbody>
+						<tr>
+							<th><span>{'Github Repository:'}</span></th>
+							<td><span>{team.githubUrl || '<none>'}</span></td>
+						</tr><tr>
+							<th><span>{'Deployed Project:'}</span></th>
+							<td><span>{team.deployedUrl || '<none>'}</span></td>
+						</tr><tr>
+							<th><span>{'Users:'}</span></th>
+							<td><span>{users || '<none>'}</span></td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		);
 	}
